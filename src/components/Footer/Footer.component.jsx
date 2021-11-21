003308655fc091c07aae3d300fc0427c99e7ee11
toantqt <toantqt0609@gmail.com>
2021-11-21 22:57:29 +0700

    add logo

--- src/components/Footer/Footer.component.jsx
+++ src/components/Footer/Footer.component.jsx
@@ -44,7 +44,7 @@ const FooterComponent = () => {
           </div>
           <div>
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3937.456042692093!2d105.7840540143279!3d9.292797087361222!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a1a75937035059%3A0x56f3e605ead1e429!2zQ8O0bmcgdHkgVHLDumMgQW5o!5e0!3m2!1svi!2s!4v1630510859229!5m2!1svi!2s"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.7996848106322!2d105.78031441433212!3d10.033382475212655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a062a00f47a4fd%3A0x849d9f8219b3512d!2zMjQsIDMwIMSQLiBMw70gVOG7sSBUcuG7jW5nLCBBbiBM4bqhYywgTmluaCBLaeG7gXUsIEPhuqduIFRoxqEsIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1637510161495!5m2!1svi!2s"
               width="100%"
               height="150"
               style={{ border: "none" }}
